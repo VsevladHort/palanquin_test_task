8d3dc95ee5261f07ad1f45df813d9de3368d6de8
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2023-09-04 15:03:42 +0300

    initial commit

---
+++ src/main/java/sample/IEventLogger.java
@@ -0,0 +1,24 @@
+package sample;
+
+import java.util.List;
+
+/**
+ * This class should be implemented to provide event logging capabilities.
+ *
+ * Objective:
+ * Implement methods to log events and get a list of all logged events in reverse order.
+ *
+ * Example:
+ * 1. log("Event1")
+ * 2. log("Event2")
+ * 3. log("Event3")
+ * 4. getEventsInReverse() should return ["Event3", "Event2", "Event1"]
+ */
+public class IEventLogger {
+    public void log(String event) {
+        // your solution here
+    }
+    public List<String> getEventsInReverse() {
+        // your solution here
+    }
+}
